96dab0b1579a163db9a8b0b811f5f7f69443a9d5
AlejandroGoper <ia.gomezperez@ugto.mx>
2021-11-17 21:31:26 -0600

    Actualizando codigos con nuevas funcionalidades

--- Mediciones/Codigo/FabryPerot/FFT_support.py
+++ Mediciones/Codigo/FabryPerot/FFT_support.py
@@ -45,43 +45,68 @@ def calcular_verdadera_amplitud(fft):
     return magnitud
 
 """
-Esta funcion calcula el espectro de amplitud normalizado respecto al numero de datos
-y con la contribucion correcta de cada una de las frecuencias positivas de la transformada de Fourier.
+Esta funcion calcula el espectro de amplitud normalizado respecto al numero de 
+datos y con la contribucion correcta de cada una de las frecuencias positivas 
+de la transformada de Fourier escalado a OPL.
 
 Devuelve:
     vfreq_positivas: array con las frecuencias positivas del espectro (eje x)
     magnitud_fft: array con la magnitud de la fft 
 """
 
-def encontrar_FFT(lambda_inicial, T_muestreo_lambda, Reflectancia):
+def encontrar_FFT_dominio_en_OPL(lambda_inicial, lambda_final, senal):
+    # Dado que suponemos que len(senal) = len(lambda_)
+    n = len(senal)
     # Al realizar el cambio de variable beta = 1/lambda, tenemos que 
-    T_muestreo_beta = T_muestreo_lambda / (lambda_inicial*(lambda_inicial+T_muestreo_lambda))
-    # Encontramos la FFT de la reflectancia
-    fft_reflectancia = fft(Reflectancia)
+    T_muestreo_beta = (1/lambda_inicial - 1/lambda_final)/n
+    
     """
-    Esta funcion calcula el vector de "frecuencias" de la transformada de fourier, vease
+        Es importante notar el escalamiento de los ejes, dado que en esta nueva 
+        variable beta, el espaciado entre cada frecuencia es del orden de 
+        10**8 nanometros, por lo que debemos dividir el vector de frecuencias 
+        por un factor de 10**6 para que convierta los nanometros a milimetros, 
+        ademas dado que en general tenemos x(beta) = cos[2pi * (2OPL) * beta] 
+        donde beta es la variable independiente beta = 1/lambda en el espectro 
+        de fourier los picos de "frecuencias" estaran ubicados en +-2OPL 
+        si queremos que cada pico de frecuencia diga el OPL directo, debemos 
+        agregar un factor de 2 adicional al vector de frecuencias
+        
+        Todo esto podemos realizarlo multiplicando T_muestreo_beta*(2*10**6) 
+    """    
+    opl, amp = encontrar_FFT(T_muestreo=T_muestreo_beta*(2*10**6),
+                                                  senal=senal)
+    return opl, amp
+"""
+Esta funcion calcula la transformada de fourier de una señal en general.
+
+Requiere:
+    T_muestreo: el periodo de muestreo entre cada medicion en el dominio 
+        antes de la FFT
+    senal: la señal a la cual se aplicara la transformada de fourier
+Devuelve:
+    vfreq_positivas: Vector de frecuencias (solo las positivas) en las unidades
+        de T_muestreo
+    magnitud_FFT: El espectro de magnitud de la FFT (solo las contribuciones 
+                                                     positivas)
+
+"""
+
+def encontrar_FFT(T_muestreo, senal):
+    n = len(senal)
+    # Encontramos la FFT de la senal
+    fft_senal = fft(senal)
+    vfreq = fftfreq(n,T_muestreo)
+    """
+     Esta funcion calcula el vector de "frecuencias" de la transformada de 
+     fourier, vease
     
     https://numpy.org/doc/stable/reference/generated/numpy.fft.fftfreq.html
     
     Es el analogo a la funcion vfreq realizada en mi github: 
         
         https://github.com/AlejandroGoper/Fundamento_de_Procesamiento_Digital_de_Senales/blob/main/Tarea5/Codigo/Tarea5.ipynb
-        
-        Es importante notar el escalamiento de los ejes, dado que en esta nueva variable beta
-        el espaciado entre cada frecuencia es del orden de 10**8 nanometros, por lo que debemos
-        dividir el vector de frecuencias por un factor de 10**6 para que convierta los nanometros 
-        a milimetros, ademas dado que en general tenemos 
-        
-        x(beta) = cos[2pi * (2OPL) * beta] donde beta es la variable independiente beta = 1/lambda
-        
-        en el espectro de fourier los picos de "frecuencias" estaran ubicados en +-2OPL 
-        
-        si queremos que cada pico de frecuencia diga el OPL directo, debemos agregar un factor de
-        1/2 adicional al vector de frecuencias
-        
-        Todo esto podemos realizarlo multiplicando T_muestreo_beta*(2*10**6) 
-    """    
-    vfreq = fftfreq(len(fft_reflectancia),(T_muestreo_beta)*((2*10**6)))    
-    magnitud_fft = calcular_verdadera_amplitud(fft=fft_reflectancia)
+    """
+    magnitud_fft = calcular_verdadera_amplitud(fft=fft_senal)
     vfreq_positivas = recorte_frec_negativas_fft(fft=vfreq)
     return vfreq_positivas, magnitud_fft
+    
--- Mediciones/Codigo/FabryPerot/Filtros_support.py
+++ Mediciones/Codigo/FabryPerot/Filtros_support.py
@@ -15,6 +15,8 @@ Referencias:
     [3] https://ipython-books.github.io/102-applying-a-linear-filter-to-a-digital-signal/
     [4] https://swharden.com/blog/2020-09-23-signal-filtering-in-python/
     [5] https://www.youtube.com/watch?v=QWUOBlAABQU
+    [6] https://community.sw.siemens.com/s/article/window-types-hanning-flattop-uniform-tukey-and-exponential
+    [7] https://download.ni.com/evaluation/pxi/Understanding%20FFTs%20and%20Windowing.pdf
 
 """
 
@@ -41,6 +43,9 @@ def ventana_de_gauss(orden, sigma):
     ==========================================================================
     Este metodo construye una ventana de hanning centrada en (N-1)/2 muestras
     donde N es el orden del filtro (importante que N sea impar)
+    
+    Segun la referencia [7] esta ventana es adecuada para el metodo de  
+    windowing en el 95% de los casos
     ==========================================================================
 """
 def ventana_de_hanning(orden):
@@ -49,8 +54,27 @@ def ventana_de_hanning(orden):
     w_n = 0.5 - 0.5*cos(2*pi*n/M)
     return w_n
 
+"""
+    ==========================================================================
+    Este metodo construye una ventana Flattop centrada en (N-1)/2 muestras
+    donde N es el orden del filtro (importante que N sea impar)
+    
+    Vease referencia [6]
+    ==========================================================================
+"""
 
-
+def ventana_flattop(orden):
+    M = orden - 1 
+    n = arange(0,M+1)
+    a_0 = 0.21557895
+    a_1 = 0.41663158
+    a_2 = 0.277263158
+    a_3 = 0.083578947
+    a_4 = 0.006947368
+    w_n = - a_1*cos(2*pi*n/M) + a_2*cos(4*pi*n/M) + ( 
+          - a_3*cos(6*pi*n/M) + a_4*cos(8*pi*n/M) )
+    w_n += a_0
+    return w_n
 """
 
 Definicion de la clase principal para la aplicacion de filtros
